52d41d8f11594b7145d93d3768f14c3296ae863e
Sanidhya Mangal <sandyrockstar19@gmail.com>
2018-01-19 23:38:42 +0530

    Added some advanced functions for matrix

--- numpy_comparisions.py
+++ numpy_comparisions.py
@@ -14,4 +14,20 @@ inches = rainfall['PRCP'].values
 
 plt.hist(inches,40)
 
+#Some boolean expression
 x = np.arange(1,6)
+print(np.less(x,3)) #compare array elements with scalar
+print(np.greater(x,3)) #greater with an scalar elements 
+print(np.less_equal(x,3)) #<= with an scalar 
+print(np.greater_equal(x,3)) #>= with scalar
+print(np.equal(x,3)) #== with a scalar 
+print(np.not_equal(2*x,x**2)) #!= with a scalar 
+
+x  = np.random.RandomState(0).randint(10,size=(3,4)) # generate a 2D array for a random state 0 in x 
+
+print(np.count_nonzero(x<6)) #check all now zeros for condition
+print(np.sum(x < 6)) # print count of all the elements is a matrix satisfying condition
+print(np.sum(x < 6, axis=1)) # on axis 1
+print(np.sum(x > 6, axis=0)) # on axis 0
+print(np.any(x < 6)) # check if any of elements in matrix follow condition
+print(np.all(x < 6)) #check if all the elements in matrix follows the condition 
