12d955ce2fe21997772809ef20628b93901d25e9
Sanidhya Mangal <sandyrockstar19@gmail.com>
2018-01-20 12:55:12 +0530

    added scatter plot

--- numpy_fancy_indexing.py
+++ numpy_fancy_indexing.py
@@ -6,6 +6,8 @@ Created on Sat Jan 20 00:04:08 2018
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn; seaborn.set()
 
 x = np.random.RandomState(42).randint(100,size = 10)
 ind = [3,4,7]
@@ -26,3 +28,14 @@ print(X[2,[2,0,1]])
 print(X[1:,[2,0,1]]) #used slicing to index array 
 mask = np.array([1,0,1,0],dtype=bool)
 print(X[row[:,np.newaxis],mask])
+
+mean=[0,0]
+cov = [[1,2],[2,5]]
+X = np.random.RandomState(42).multivariate_normal(mean,cov,100)
+print(X)
+plt.scatter(X[:, 0], X[:, 1])
+indices = np.random.choice(X.shape[0],20,replace=False)
+indices
+selection = X[indices] #fancy indexing 
+plt.scatter(X[:, 0], X[:, 1], alpha=0.3)
+plt.scatter(selection[:, 0], selection[:, 1],s=200);
