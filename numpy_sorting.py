ab39d2eed3cf47e2b74268101b9f24778c7f9ab2
Sanidhya Mangal <sandyrockstar19@gmail.com>
2018-01-21 19:28:14 +0530

    Sorting along rows and coloumns

--- numpy_sorting.py
+++ numpy_sorting.py
@@ -11,3 +11,9 @@ print(np.sort(x)) # to sort the arrays
 print(np.argsort(x)) # to return the index of sorted elements 
 i = x.argsort() # to return index of sorted elements 
 print(x[i]) # to print sorted array using a fancy indexing
+
+#Sorting along rows and coloumns 
+
+x= np.random.RandomState(42).randint(0,10,(4,6))
+print(np.sort(x,axis=0)) # sorting along axis 0 of matrix
+print(np.sort(x,axis=1)) #sorting along axis 1 of matrix
