34ff9e36563fa3fd82fb876c86ff3dfdf1e8adda
Sanidhya Mangal <sandyrockstar19@gmail.com>
2018-01-21 20:08:18 +0530

    Added K nearest neighbour

--- numpy_sorting.py
+++ numpy_sorting.py
@@ -5,7 +5,8 @@ Created on Sat Jan 20 13:14:29 2018
 @author: Sanidhya
 """
 import numpy as np
-
+import matplotlib.pyplot as plt
+import seaborn; seaborn.set()
 x = np.array([2,1,5,6,3,4,0])
 print(np.sort(x)) # to sort the arrays 
 print(np.argsort(x)) # to return the index of sorted elements 
@@ -23,3 +24,19 @@ print(np.partition(x,3,axis=0)) #partitoned array along axis 0 around 3
 print(np.partition(x,2,axis=1)) #partitioned array along axis 1 around 2
 y = np.array([7, 2, 3, 1, 6, 5, 4])
 print(y.partition(2)) #partioned array around 2
+# K nearest neighbour 
+X = np.random.RandomState(42).rand(10,2)
+plt.scatter(X[:,0],X[:,1],s=100)
+dist_sq = np.sum((X[:, np.newaxis, :] - X[np.newaxis, :, :]) ** 2, axis=-1)
+nearest = np.argsort(dist_sq,axis=1)
+print(nearest)
+
+K = 2
+nearest_partition = np.argpartition(dist_sq, K + 1, axis=1)
+
+plt.scatter(X[:,0],X[:,1],s=100)
+K = 2
+for i in range(X.shape[0]):
+    for j in nearest_partition[i,:K+1]:
+        
+        plt.plot(*zip(X[j],X[i]),color='black')
