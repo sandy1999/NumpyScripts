c103f0a3b875e96bfd5822dfaa843e1f3326d4e7
Sanidhya Mangal <sandyrockstar19@gmail.com>
2018-01-21 22:15:37 +0530

    Compound data type

--- numpy_structured_array.py
+++ numpy_structured_array.py
@@ -22,3 +22,10 @@ print(data)
 
 print(data[2:]) #slicing data after index 2
 print(data[np.less(data['age'],20)]['name']) #printing name of people of age < 20
+
+#more advanced compound types 
+
+tp = np.dtype([('id','i8'),('mat','f8',(3,3))])
+X = np.zeros(1,dtype=tp)
+print(X) #print a complete array
+print(X['mat'][0]) #print an array with zero index and of mat property
